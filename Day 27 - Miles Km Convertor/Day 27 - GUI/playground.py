405c084754d1f00b779aab4a070e670aadeb0bd3
stephen <stephenlancaster1981@hotmail.co.uk>
2025-02-12 11:36:15 +0000

    Added work from day 27 - a miles to km / km to miles convertor project

---
+++ Day 27 - Miles Km Convertor/Day 27 - GUI/playground.py
@@ -0,0 +1,12 @@
+# def add(*args):
+#     total = 0
+#     for n in args:
+#         total+= n
+#     return total
+#
+# add(2,3,4,5,6)
+#
+# def calculate(**kwargs):
+#     print (kwargs)
+#
+# calculate(add = 1 , multipy = 5)
