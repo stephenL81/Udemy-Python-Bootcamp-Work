de307a732fde57a3d517ad3fdd9d03168ab805f1
stephen <stephenlancaster1981@hotmail.co.uk>
2026-01-29 16:22:30 +0000

    updating missing files for day 28

---
+++ test.py
@@ -0,0 +1,11 @@
+TEST = "✔"
+
+TEST += TEST
+
+print(TEST)
+
+#so after the first "work" a tick appears
+#after the second another tick appears - 2 total
+#after the third 2 appears - 4 total
+#after the fourth - 4 appear - 8 total
+#after the long break 8 appear - 16 total
